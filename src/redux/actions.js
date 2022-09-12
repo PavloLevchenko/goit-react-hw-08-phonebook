10975f5c1ffc52fcc06a923ac21484730abff029
PavloLevchenko <teasetrnet@gmail.com>
2022-09-12 21:12:19 +0200

    Change catch, remove junk

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,28 +5,27 @@ import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = "https://631f391558a1c0fe9f63c113.mockapi.io/v1";
 
 export const getContacts = createAsyncThunk('contacts/get', async () => {
-    const { data } = await axios('/contacts')
-    .catch(function (error) {
-      toast(error.message);
-    });
-    return data;
+const { data } = await axios('/contacts')
+  .catch(function (error) {
+    toast(error.message);
+  });
+  return data;
 });
 
 export const addContact = createAsyncThunk('contacts/add', async ({name, phone}) => {
-    const { data } = await axios.post('/contacts', {name, phone})
-    .catch(function (error) {
-      toast(error.message);
-    });
-    return data;
+const { data } = await axios.post('/contacts', {name, phone})
+  .catch(function (error) {
+    toast(error.message);
+  });
+  return data;
 });
 
 export const removeContact = createAsyncThunk('contacts/remove', async id => {
-  try {
-    const { data } = await axios.delete('/contacts/' + id);
-    return data;
-  } catch (error) {
+const { data } = await axios.delete('/contacts/' + id)
+  .catch(function (error) {
     toast(error.message);
-  }
+  });
+  return data;
 });
 
 export const setFilter = createAction('contacts/filter');
--- src/redux/middleware.js
+++
@@ -1,7 +0,0 @@
-import {FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER} from 'redux-persist';
-
-export const middleware = {
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  };
